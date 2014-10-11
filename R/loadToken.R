dc5dd54eb38bb976e3e507544be44dc21bd4df47
Matthias Bannert <matthias.bannert@gmail.com>
2014-10-11 14:02:54 +0200

    improved temporary saving of auth files

--- R/loadToken.R
+++ R/loadToken.R
@@ -14,33 +14,20 @@ loadToken = function(credlist) {
   # Returns:
   #   access.token with corresponding time stamp
   opts = list(verbose=T)
-  a = fromJSON(postForm("https://accounts.google.com/o/oauth2/token", .opts=opts, code=credlist$c.token, client_id=credlist$c.id,
-                        client_secret=credlist$c.secret, redirect_uri="urn:ietf:wg:oauth:2.0:oob", grant_type="authorization_code", 
+  a = fromJSON(postForm("https://accounts.google.com/o/oauth2/token",
+                        .opts=opts, code=credlist$c.token,
+                        client_id=credlist$c.id,
+                        client_secret=credlist$c.secret,
+                        redirect_uri="urn:ietf:wg:oauth:2.0:oob",
+                        grant_type="authorization_code", 
                         style="POST"))
+  
   if (length(a) == 1) {
-    print('You need to update the token - run getAuth()')
+    print('You need to update the token - run doAuth()')
   } else {
-    access.token <- list()
-    access.token$timeStamp <- as.numeric(Sys.time())
-    access.token.obj <- a 
-    # make sure your credentials are ignored by svn and git ####
-    if (!file.exists(".gitignore")){
-      cat(".access.token.RData",file=".gitignore",sep="\n")
-    } 
-    if (file.exists(".gitignore")){
-      cat(".access.token.RData",file=".gitignore",append=TRUE)
-    }
-    if (file.exists(".svnignore")){
-      cat(".access.token.RData",file=".svnignore",append=TRUE)
-    }
-    if (!file.exists(".svnignore")){
-      cat(".access.token.RData",file=".svnignore",append=TRUE)
-    }
-    
-    save(access.token, file=".access.token.RData")
-    
+    a$timeStamp <- as.numeric(Sys.time())
   }
   
-  access.token
+  a
   
 }
