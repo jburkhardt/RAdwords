831fd738e6030f5a4f9f9c92ca4e57dbe0c2e540
jburkhardt <johannes.burkhardt@gmail.com>
2016-01-12 10:46:52 +0100

    bug fix in reports()

--- R/reports.R
+++ R/reports.R
@@ -2,32 +2,25 @@
 #' 
 #' @description reports provides an overview of all available Adwords report types. The report type is specified in \code{\link{statement}}.
 #' 
-#' @param apiVersion Supports 201506 or 201502. Default is 201506.
+#' @param apiVersion Supports 201509 or 201506. Default is 201509.
 #' 
 #' @export
 #' @return Available report types.
-reports <- function(apiVersion="201506"){
+reports <- function(apiVersion="201509"){
 # Function which returns all available report types
 #
 # Args: None
 #
 # Returns:
 #     Report types
-if (apiVersion=="201506"){
-    reportTypes <- list.files(system.file(package="RAdwords",'extdata/api201506/'))
-    reportTypes <- sub('.csv','',reportTypes)
-    reportTypes <- gsub('-','_',reportTypes)
-    reportTypes <- toupper(reportTypes)
+  if (apiVersion=="201509"){
+      reportTypes <- list.files(system.file(package="RAdwords",'extdata/api201509/'))
   }
-else if (apiVersion=="201502"){
-  reportTypes <- list.files(system.file(package="RAdwords",'extdata/api201502/'))
+  else if (apiVersion=="201506"){
+      reportTypes <- list.files(system.file(package="RAdwords",'extdata/api201506/'))
+  }
   reportTypes <- sub('.csv','',reportTypes)
   reportTypes <- gsub('-','_',reportTypes)
   reportTypes <- toupper(reportTypes)
-}
-else if (apiVersion=="201409"){
-  reportTypes <- list.files(system.file(package="RAdwords",'extdata/api201409/'))
-  reportTypes <- sub('.csv','',reportTypes)
-}
-reportTypes
+  reportTypes
 }
