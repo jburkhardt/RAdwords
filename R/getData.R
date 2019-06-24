71e0776aee5de0e5f5ca9620c7cc8a7db53ff8a5
jburkhardt <johannes.burkhardt@gmail.com>
2019-06-24 13:19:06 +0200

    refactor purrr:map to lapply

--- R/getData.R
+++ R/getData.R
@@ -5,7 +5,7 @@
 #' @description getData posts the Adwords Query Language (awql) Statement which is generated with \code{\link{statement}}.
 #' The data are retrieved from the Adwords API as a dataframe.
 #' 
-#' @param clientCustomerId Adwords Account Id
+#' @param clientCustomerId Adwords Account Id; supports a single account id: "xxx-xxx-xxxx" or a vector of ids from the same Google Ads MCC: c("xxx-xxx-xxxx", "xxx-xxx-xxxx")
 #' @param google_auth list of authentication
 #' @param statement awql statement generated with \code{\link{statement}}.
 #' @param apiVersion supports 201809, 201806, 201802 defaults to 201806.
@@ -14,8 +14,6 @@
 #' @param includeZeroImpressions If TRUE zero impressions will be included. Defaults to FALSE.
 #' @param changeNames If TRUE, the display names of the transformed data are converted into more nicer/practical names. Requires transformation = TRUE
 #' @param verbose Defaults to FALSE. If TRUE, the curl connection output will be printed.
-#' 
-#' @importFrom purrr map
 #' 
 #' @export
 #' 
@@ -28,28 +26,18 @@ getData <- function(clientCustomerId,
                     changeNames=TRUE,
                     includeZeroImpressions=FALSE,
                     verbose=FALSE){
-  if(length(clientCustomerId)==1){
-    data <- .getDataHelper(clientCustomerId = clientCustomerId,
-                           google_auth = google_auth,
-                           statement = statement,
-                           apiVersion = apiVersion,
-                           transformation = transformation,
-                           changeNames = changeNames,
-                           includeZeroImpressions = includeZeroImpressions,
-                           verbose = verbose)
-  } else {
-    data_list <- purrr::map(.x = clientCustomerId,
-                            .f = function(.x){.getDataHelper(clientCustomerId = .x,
-                                                             google_auth = google_auth,
-                                                             statement = statement,
-                                                             apiVersion = apiVersion,
-                                                             transformation = transformation,
-                                                             changeNames = changeNames,
-                                                             includeZeroImpressions = includeZeroImpressions,
-                                                             verbose = verbose)})
-    data <- do.call(rbind, data_list)
-    
-  }
-  
+  # applies .getDataHelper for each account and saves data in list object
+  data_list <- lapply(X = clientCustomerId,
+                      FUN = .getDataHelper,
+                      google_auth = google_auth,
+                      statement = statement,
+                      apiVersion = apiVersion,
+                      transformation = transformation,
+                      changeNames = changeNames,
+                      includeZeroImpressions = includeZeroImpressions,
+                      verbose = verbose)
+  # binds list to dataframe
+  data <- do.call(rbind, data_list)
+  # return
   data
 }
